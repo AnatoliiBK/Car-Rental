c37c29b445cf87b91442490bb6ba830848667ebd
AnetRo <beatrice.ccq@gmail.com>
2023-10-18 00:53:53 +0300

    app & sidebar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from "react";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Switch,
+//   Redirect,
+// } from "react-router-dom";
+// import HomePage from "./pages/HomePage";
+// import CatalogPage from "./pages/CatalogPage";
+// import FavoritesPage from "./pages/FavoritesPage";
+import { Sidebar } from "./components/Sidebar";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Sidebar />
+      {/* <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/catalog" component={CatalogPage} />
+          <Route exact path="/favorites" component={FavoritesPage} />
+          <Redirect to="/" />
+        </Switch>
+      </Router> */}
+    </>
   );
-}
+};
 
-export default App;
+// import logo from "./logo.svg";
+// import "./App.css";
+
+// export const App = () => {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// };
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <form>
+                <div className="form-group">
+                    <label>Car brand:</label>
+                    <input type="text" placeholder="Enter the text" />
+                    <button>Brand</button>
+                </div>
+                <div className="form-group">
+                    <label>Price/ 1 hour:</label>
+                    <input type="text" placeholder="To $" />
+                    <button>Price</button>
+                </div>
+                <div className="form-group">
+                    <label>Car mileage / km:</label>
+                    <input type="text" placeholder="From" />
+                    <input type="text" placeholder="To" />
+                </div>
+                <button type="submit">Search</button>
+            </form>
+        </div>
+    );
+};
+
