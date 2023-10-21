c113311cbd5a9096bb2add11d7ae569a67dd29cc
AnetRo <beatrice.ccq@gmail.com>
2023-10-21 16:40:48 +0300

    label

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,17 +5,17 @@ export const Sidebar = () => {
         <div className="sidebar">
             <form>
                 <div className="form-group">
-                    <label>Car brand:</label>
+                    <label className='label'>Car brand:</label>
                     <input type="text" placeholder="Enter the text" />
                     <button>Brand</button>
                 </div>
                 <div className="form-group">
-                    <label>Price/ 1 hour:</label>
+                    <label className='label'>Price/ 1 hour:</label>
                     <input type="text" placeholder="To $" />
                     <button>Price</button>
                 </div>
                 <div className="form-group">
-                    <label>Car mileage / km:</label>
+                    <label className='label'>Car mileage / km:</label>
                     <input type="text" placeholder="From" />
                     <input type="text" placeholder="To" />
                 </div>
