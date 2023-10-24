d80687678e76689d319efa834e53a55c3e9f2b67
AnetRo <beatrice.ccq@gmail.com>
2023-10-25 00:26:50 +0300

    HeroTitle

---
+++ src/components/HeroTitle.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const HeroTitle = ({title}) => {
+    return (
+        <h1 className="heroTitle">{title}</h1>
+    )
+}
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Button } from "../Button";
 import { Section } from "../sections/Section";
-
+import { HeroTitle } from "../HeroTitle";
 export const HomePage = () => {
     return (
         <div className="homePage">
             <Section>
-                <h1 className="heroTitle"> Do you want an incredible experience? Choose a car for yourself! Every single one of them is great!</h1>
+                <HeroTitle title='Do you want an incredible experience? Choose a car for yourself! Every single one of them is great!' />
+                {/* <h1 className="heroTitle"> Do you want an incredible experience? Choose a car for yourself! Every single one of them is great!</h1> */}
                 <Button text='All Cars' />
                 <Button text='Sporting Cars' />
                 <Button text='SUV Cars' />
