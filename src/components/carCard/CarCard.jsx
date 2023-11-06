5d3e13aa83dfb7ed23a204071b2ba9704d3dc7d0
AnetRo <beatrice.ccq@gmail.com>
2023-11-06 21:24:26 +0200

    gret it

--- src/components/carCard/CarCard.jsx
+++ src/components/carCard/CarCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { nanoid } from 'nanoid';
 
 export const CarCard = ({ car }) => {
   return (
@@ -14,14 +15,14 @@ export const CarCard = ({ car }) => {
       <p>Mileage: {car.mileage}</p>
       <h3>Accessories:</h3>
       <ul>
-        {car.accessories.map((item, index) => (
-          <li key={index}>{item}</li>
+        {car.accessories.map((item) => (
+          <li key={nanoid()}>{item}</li>
         ))}
       </ul>
       <h3>Functionalities:</h3>
       <ul>
-        {car.functionalities.map((item, index) => (
-          <li key={index}>{item}</li>
+        {car.functionalities.map((item) => (
+          <li key={nanoid()}>{item}</li>
         ))}
       </ul>
       <p>Rental Conditions: {car.rentalConditions}</p>
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { Button } from "../Button";
 import { Section } from "../sections/Section";
 import { HeroTitle } from "../HeroTitle";
+import { NavLink } from "react-router-dom"; // імпортувати компонент Link
+
 const HomePage = () => {
     return (
         <div className="homePage">
             <Section>
                 <HeroTitle title='Do you want an incredible experience? Choose a car for yourself! Every single one of them is great!' />
                 {/* <h1 className="heroTitle"> Do you want an incredible experience? Choose a car for yourself! Every single one of them is great!</h1> */}
-                <Button text='All Cars' />
+                <NavLink to='/catalog'>
+                    <Button text='All Cars' />
+                </NavLink>
                 <Button text='Sporting Cars' />
                 <Button text='SUV Cars' />
                 <Button text='All Road Cars' />
