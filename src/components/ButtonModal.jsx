c3f206b5d2bc25fb6285cb6b5a2e85a735002e01
AnetRo <beatrice.ccq@gmail.com>
2023-11-19 15:24:22 +0200

    modal des

--- src/components/ButtonModal.jsx
+++ src/components/ButtonModal.jsx
@@ -1,8 +1,11 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useMemo} from "react";
 import { CarCard } from "./carCard/CarCard";
+import axios from "axios";
 
-export const ButtonModal = ({ children }) => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+export const ButtonModal = ({ children, carId }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [cars, setCars] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -11,6 +14,28 @@ export const ButtonModal = ({ children }) => {
   const closeModal = () => {
     setIsModalOpen(false);
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://652ed5ac0b8d8ddac0b1f589.mockapi.io/adverts');
+        const fetchedAdverts = response.data;
+        console.log("FETCH", fetchedAdverts)
+
+        setCars(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching data', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const selectedCar = useMemo(() => {
+    return cars.find(car => car.id === carId);
+  }, [cars, carId]);
+
     return (
     <div>
       <button className="btn-img" onClick={openModal}>{children}</button>
@@ -18,8 +43,12 @@ export const ButtonModal = ({ children }) => {
       {isModalOpen && (
         <div className="modal-overlay" onClick={closeModal}>
           <div className="modal" onClick={(e) => e.stopPropagation()}>
-            <CarCard />
-            <button onClick={closeModal}>Close Modal</button>
+          {!isLoading && (
+            <CarCard car={selectedCar} />
+          )}
+        
+      
+            <button onClick={closeModal}>LEAVE THIS</button>
           </div>
         </div>
       )}
--- src/components/carCard/CarData.jsx
+++ src/components/carCard/CarData.jsx
@@ -1,42 +1,24 @@
 import React from 'react';
-// import { nanoid } from 'nanoid';
-// import { SeeMore } from './SeeMore';
 import { ButtonModal } from '../ButtonModal';
 
-export const CarData = ({ car }) => {
+export const CarData = ({ car, carId }) => {
     const companyAddress = car.address.split(',');
     const city = companyAddress[1];
     const country = companyAddress[2];
     return (
     <div className="car-card">
-      <ButtonModal>
+      <ButtonModal carId={car.id}>
         <img className="car-img" src={car.img} alt={`${car.make} ${car.model}`} />
       </ButtonModal>
       <h2>{`${car.make} ${car.model}`}</h2>
-      {/* <SeeMore text='See more' /> */}
+      
       <div className='descr'>
         <p>Rental Price: {car.rentalPrice}</p>
-        {/* <p>{car.description}</p>
-        <p>Fuel Consumption: {car.fuelConsumption}</p>
-        <p>Engine Size: {car.engineSize}</p> */}
-        
         <p>Rental Company: {car.rentalCompany}</p>
         <p>{city} {country}</p>
       </div>
       <h3>{car.year} Mileage: {car.mileage}</h3>
-      {/* <h3>Accessories:</h3>
-      <ul>
-        {car.accessories.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-      <h3>Functionalities:</h3>
-      <ul>
-        {car.functionalities.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul> */}
-      {/* <p>Rental Conditions: {car.rentalConditions}</p> */}
+      
     </div>
   );
 };
